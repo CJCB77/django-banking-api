b11e27bb647f775e7dd72e4c47c6c11aa24f2848
Johnny Coral <coral.johnny.2@gmail.com>
2025-08-08 22:46:11 -0500

    Configured base settings project file

---
+++ config/settings/local.py
@@ -0,0 +1,10 @@
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-yu=emzj&6p18x@-o9&r6#+^w&@&2g7=d%497^1k9q@*)*2ps7@"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
